e52c0774647437b4c52b9e949ec6f242b0c78d09
Ittichai Boonyarakthunya <i.boonyarakthunya@gmail.com>
2024-09-13 17:45:03 +0700

    set up dl k-fold

--- utils/constant.py
+++ utils/constant.py
@@ -6,3 +6,12 @@ UTILS_DIR = '../../utils'
 NLTK_DATA_PATH = f"{FINE_TUNED_DIR}/nltk_data"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, '..'))
+
+
+embedding_dim = 300  # Assuming Word2Vec has 300 dimensions
+gru_units = 128
+dropout_rate = 0.5
+num_classes = 1  # Binary classification
+max_sequence_length = 100  # Maximum length of input sequences
+ngram_range = (1, 2)  # Unigrams and bigrams
+max_features = 20000  
